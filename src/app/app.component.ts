7587e40230384fa24d3738cfb77e2e32a3b13757
Anna <annamotsikula@gmail.com>
2024-04-25 21:38:03 +0400

    first touch in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // template: '<h2>Angular Demo Title</h2>',
+  styleUrl: './app.component.scss',
+  // styles: [
+  //   'h1 {color: red};'
+  // ]
 })
 export class AppComponent {
   title = 'ng-project-04';
